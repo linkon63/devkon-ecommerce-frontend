7588caecaab7034a9756f6518117604962f8c2d4
linkon063 <linkon@softzino.com>
2025-06-02 07:54:17 +0600

    bdt

--- src/constants/priceRange.ts
+++ src/constants/priceRange.ts
@@ -1,8 +1,8 @@
 import { TPriceRange } from "@/types/priceRangeType";
 
 export const priceRanges: TPriceRange[] = [
-  { label: "$0 - $60", min: 0, max: 60 },
-  { label: "$60 - $300", min: 60, max: 300 },
-  { label: "$300 - $800", min: 300, max: 800 },
-  { label: "Above $800", min: 800, max: null },
+  { label: "TK:0 - TK:60", min: 0, max: 60 },
+  { label: "TK:60 - TK:300", min: 60, max: 300 },
+  { label: "TK:300 - TK:800", min: 300, max: 800 },
+  { label: "Above TK:800", min: 800, max: null },
 ];
